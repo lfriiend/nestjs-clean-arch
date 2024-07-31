4d68af4d1e357eff4224355e6349be9cbfd35aeb
Lucas Sales <103463820+lfriiend@users.noreply.github.com>
2024-07-31 06:39:42 -0300

    EnvConfigService unit tests

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,10 @@ import { Module } from '@nestjs/common'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { EnvConfigModule } from './shared/infrastructure/env-config/env-config.module'
-import { EnvConfigService } from './shared/infrastructure/env-config/env-config.service'
 
 @Module({
   imports: [EnvConfigModule],
   controllers: [AppController],
-  providers: [AppService, EnvConfigService],
+  providers: [AppService],
 })
 export class AppModule {}
